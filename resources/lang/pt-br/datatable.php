1ddb4b13fb36ef4e1525a143555d5df631d9dfa0
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2021-04-19 18:13:03 -0500

    Base package

---
+++ resources/lang/pt-br/datatable.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'export' => 'Exportar',
+    'loading' => 'Carregando...',
+    'search' => 'Procure em :columns',
+    'select_all' => 'Selecionar tudo',
+    'no_data_table' => 'Não há nada para mostrar no momento',
+    'pagination_text' => 'Resultados :start - :end de :total',
+    'prev' => 'Ant',
+    'next' => 'Seg',
+    'all' => 'Tudo',
+    'hide' => 'Ocultar',
+    'show' => 'Mostrar',
+    'table_col' => 'Colunas da Tabela',
+    'no' => 'Não',
+    'yes' => 'Sim',
+    'min' => 'Min',
+    'max' => 'Max',
+    'choose' => 'Selecionar...',
+    'type_find' => 'Digite aqui para encontrar',
+    'modal_delete_title' => 'Deletar :id',
+    'modal_delete_info' => 'Tem certeza?',
+    'deleting' => 'Deletando...'
+];
