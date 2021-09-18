9f0ad6b9be7a266c2ee00879392e7868dfd3aa53
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2021-09-18 15:11:02 -0500

    Lang FR

---
+++ resources/lang/fr/datatable.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'export' => 'Exporter',
+    'loading' => 'Chargement...',
+    'search' => 'Rechercher dans :columns',
+    'select_all' => 'Sélectionner Tout',
+    'no_data_table' => 'Il n\'y a rien à afficher pour le moment',
+    'pagination_text' => 'Résultats :start - :end of :total',
+    'prev' => 'Pré',
+    'next' => 'Pro',
+    'all' => 'Tout',
+    'hide' => 'Masquer',
+    'show' => 'Visualiser',
+    'table_col' => 'Colonnes du tableau',
+    'no' => 'Non',
+    'yes' => 'Oui',
+    'min' => 'Min',
+    'max' => 'Max',
+    'choose' => 'Choisir...',
+    'type_find' => 'Tapez ici pour rechercher',
+    'modal_delete_title' => 'Supprimer :id',
+    'modal_delete_info' => 'Êtes-vous sûr?',
+    'deleting' => 'Suppression...'
+];
